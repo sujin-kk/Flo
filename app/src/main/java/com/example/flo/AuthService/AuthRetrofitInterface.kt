ce1308c46adf21854bbe0413d9b6d10e90ca1d8d
임수진 <sjoo0002@gmail.com>
2022-03-13 15:17:45 +0900

    refoldering

--- app/src/main/java/com/example/flo/AuthService/AuthRetrofitInterface.kt
+++ app/src/main/java/com/example/flo/AuthService/AuthRetrofitInterface.kt
@@ -8,12 +8,15 @@ import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface AuthRetrofitInterface {
+    // user data
     @POST("/users")
     fun signUp(@Body user: User): Call<AuthResponse>
 
+    // app login
     @POST("/users/login")
     fun login(@Body user: User): Call<AuthResponse>
 
+    // auto login
     @GET ("/users/auto-login")
     fun autoLogin(@Header("X-ACCESS-TOKEN") jwt: String) : Call<AuthResponse>
 }
